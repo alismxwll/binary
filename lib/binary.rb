d90bf1f5dca2a46adc6a3dc94af86b9f31c19ab6
Andrew Robert Dean Akers <student@epicodus.com>
2014-07-28 16:57:53 -0700

    initial commit

---
+++ lib/binary.rb
@@ -0,0 +1,9 @@
+def binary(number)
+number = number.to_s
+result = number.to_i(2)
+
+puts result
+
+end
+
+binary(101111111111)
---
+++ lib/hexadecimal.rb
@@ -0,0 +1,9 @@
+def binary(number)
+number = number.to_s(16)
+# result = number.to_i(16)
+
+puts number
+
+end
+
+binary(16)
